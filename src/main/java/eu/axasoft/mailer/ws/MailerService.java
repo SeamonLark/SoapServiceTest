4390ce41df3e40068853cd6df4ec9756428a135f
Ondro Mihalyi <mihalyi@omnifish.ee>
2025-08-28 13:59:57 +0200

    Clean up the application
    - remove servlet mapping and servlet listener, will use the default behavior in GlassFish
    - remove @Stateless annotation which was previously ignored anyway. It can be added back, but it would change the URL of the service, because GlassFish maps EJB web services to a separate application context, e.g. /MailerService/MailerService instead of /APPLICATION_CONTEXT/MailerService, in this case /SoapServiceTest/MailerService
    - removed unused sun-jaxws.xml. It was required by the WSServletContextListener servlet context listener mapped in web.xml
    - removed the JAX-WS dependencies from pom.xml. The implementation is not necessary at all, it's provided by GlassFish and not needed at compile time. The API should have provided scope, so that it doesn't get bundled to the WAR. And it's already included in the Jakarta EE API dependency, so it's not needed

--- src/main/java/eu/axasoft/mailer/ws/MailerService.java
+++ src/main/java/eu/axasoft/mailer/ws/MailerService.java
@@ -1,17 +1,21 @@
 package eu.axasoft.mailer.ws;
 
+
 import eu.axasoft.soapsettest.core.ApplicationBean;
 import eu.axasoft.soapsettest.core.EjbTestLocal;
+
 import java.util.Date;
+
 import jakarta.annotation.Resource;
 import jakarta.ejb.EJB;
-import jakarta.ejb.Stateless;
+import jakarta.enterprise.inject.spi.CDI;
 import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.ws.WebServiceContext;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,22 +24,25 @@ import java.util.logging.Logger;
  * @author balaz
  */
 @WebService(serviceName = "MailerService")
-@Stateless
+//@Stateless
 public class MailerService {
 
     private static final Logger LOG = Logger.getLogger(MailerService.class.getName());
-    
+
     @Resource
     private WebServiceContext context;
-    
+
     @Inject
     private ApplicationBean appBean;
     @EJB
     private EjbTestLocal ejbTest;
 
+    public MailerService() {
+    }
+
     /**
-     * Stores the plain or html email into the database. 
-     * 
+     * Stores the plain or html email into the database.
+     *
      * @param destinationAddress destination address or addresses separated by comma
      * @param sourceAddress sender of the email
      * @param subject subject of the email
@@ -47,9 +54,9 @@ public class MailerService {
      */
     @WebMethod(operationName = "storeEmail")
 
-    public Integer storeEmail(        
+    public Integer storeEmail(
         @WebParam(name = "destinationAddress") String destinationAddress,
-        @XmlElement(required = false) @WebParam(name = "sourceAddress") String sourceAddress, 
+        @XmlElement(required = false) @WebParam(name = "sourceAddress") String sourceAddress,
         @XmlElement(required = false) @WebParam(name="subject") String subject,
         @WebParam(name="content") String content,
         @WebParam(name="htmlContent") boolean htmlContent
@@ -67,40 +74,40 @@ public class MailerService {
         if (appBean == null) {
             nullCount++;
         }
-        
+
         return nullCount;
     }
-    
+
     /**
-     * Returns the email status. 
+     * Returns the email status.
      * @param emailId email database id
      * @return String describing email status {NEW, INPC, ERR, SENT}
      */
     @WebMethod(operationName = "getEmailStatus")
 
     public String getEmailStatus(@WebParam(name = "emailId") Integer emailId) {
-        return "";
+        return " Injected AppBean: " + appBean + "; Retrieved AppBean: " + CDI.current().select(ApplicationBean.class).get();
     }
-    
+
     /**
-     * actually do nothing!  
+     * actually do nothing!
      */
     @WebMethod(operationName = "sendError" )
 
     public void sendError( @WebParam( name = "emailAddress" ) String emailAddress ){
-        
+
     }
 
     /**
-     * Stores the plain or html email into the database. This version supports 
+     * Stores the plain or html email into the database. This version supports
      * also attachments, which are separated by semicolon (;)
-     * 
+     *
      * @param destinationAddress destination address or addresses separated by comma
      * @param sourceAddress sender of the email
      * @param subject subject of the email
      * @param content plain text or html content
      * @param htmlContent differs if the content is html content or plain text
-     * @param pathToAttachments optional parateter which contais the semicolon separated paths 
+     * @param pathToAttachments optional parateter which contais the semicolon separated paths
      * to the email attachments. Attachment must exist in the time of calling this service.
      * @return if the operation succeeds email id is returned. Otherwise exception is thrown.
      * @throws MailerServiceException Wraps catched excepions.
@@ -108,12 +115,12 @@ public class MailerService {
     @WebMethod(operationName = "storeEmailWithAttachment")
 
     public Integer storeEmailWithAttachment(
-        @WebParam(name = "destinationAddress") String destinationAddress, 
-        @XmlElement(required = false) @WebParam(name = "sourceAddress") String sourceAddress, 
-        @XmlElement(required = false) @WebParam(name = "subject") String subject, 
-        @WebParam(name = "content") String content, 
-        @WebParam(name = "htmlContent") boolean htmlContent, 
-        @XmlElement(required = false) @WebParam(name = "pathToAttachments") String pathToAttachments) 
+        @WebParam(name = "destinationAddress") String destinationAddress,
+        @XmlElement(required = false) @WebParam(name = "sourceAddress") String sourceAddress,
+        @XmlElement(required = false) @WebParam(name = "subject") String subject,
+        @WebParam(name = "content") String content,
+        @WebParam(name = "htmlContent") boolean htmlContent,
+        @XmlElement(required = false) @WebParam(name = "pathToAttachments") String pathToAttachments)
     {
         LOG.log(Level.FINE, "Values passed from caller: destinationAdddress: {0} sourceAddress: {1}, subject: {2}, content: {3}, html content: {4}, attachment path: {5}",
             new Object[] {destinationAddress, sourceAddress, subject, content, htmlContent, pathToAttachments});
@@ -121,5 +128,5 @@ public class MailerService {
 
         return 0;
     }
-    
+
 }
